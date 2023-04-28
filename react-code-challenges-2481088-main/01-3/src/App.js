4af7bc991438c3665a2345f9e204ba3803d6f13c
Igor Mashnikov <igor_mashnikov@epam.com>
2023-04-28 15:14:20 +0200

    added 1-03 being step

---
+++ react-code-challenges-2481088-main/01-3/src/App.js
@@ -0,0 +1,21 @@
+import './App.css'
+import FormValidator from './03-form-validator/FormValidator'
+
+function App() {
+  return (
+
+    <div className='App'>
+      information fits the following requirements:
+      <ul>
+        <li>The email, password, and password confirmation fields are filled</li>
+        <li>The email field must have exactly one @ sign in it</li>
+        <li>The password must be eight or more characters long</li>
+        <li>The password and password confirmation must be the same</li>
+      </ul>
+      <FormValidator />
+
+    </div>
+  )
+}
+
+export default App
