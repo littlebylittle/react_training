220aa9dc0193b68c9eaa1f6df76c7cfaded1ba1b
Igor Mashnikov <igor_mashnikov@epam.com>
2023-04-28 15:09:19 +0200

    1-02 end step added

--- react-code-challenges-2481088-main/01-2/src/02-dark-mode/DarkMode.js
+++ react-code-challenges-2481088-main/01-2/src/02-dark-mode/DarkMode.js
@@ -1,8 +1,24 @@
-export default function DarkMode () {
+import { useState, useEffect } from "react"
+import React from "react"
+
+export default function DarkMode() {
+  const [darkMode, setDarkMode] = useState(false)  //by default darkMode is off
+  const [mainClassTags, setTags] = useState("page ")
+
+  //debug purpose only
+  useEffect(() => {
+    console.log(`Darkmode flag is: ${darkMode}`);
+    if (!darkMode) {
+      setTags("page dark-mode");
+    } else {
+      setTags("page");
+    }
+  }, [darkMode]);
+
   return (
-    <div className='page'>
-      <button className='dark-mode-button'>Dark Mode</button>
-      <button className='light-mode-button'>Light Mode</button>
+    <div className={mainClassTags}>
+      <button className='dark-mode-button' onClick={() => { setDarkMode(true) }} >Dark Mode</button>
+      <button className='light-mode-button' onClick={() => { setDarkMode(false) }} >Light Mode</button>
     </div>
   )
 }
