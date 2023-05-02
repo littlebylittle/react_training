df2ff33bae55f416bedd955ca0d8ff086f5c8387
Igor Mashnikov <igor_mashnikov@epam.com>
2023-05-02 19:14:39 +0200

    01-10 useRef + useEffect

--- react-code-challenges-2481088-main/01-10/src/10-focus-input/FocusInput.js
+++ react-code-challenges-2481088-main/01-10/src/10-focus-input/FocusInput.js
@@ -1,8 +1,16 @@
+import {useEffect, useRef} from 'react'
+
 export default function FocusInput() {
+  const focusedInput = useRef(null);
+  useEffect(()=>{
+    focusedInput.current.focus();
+  }, []);
+
   return (
     <div>
       <label htmlFor='focused-input'>Focus me on page load!</label>
-      <input name='focused-input'></input>
+      {/* attribute REF! */}
+      <input name='focused-input' ref={focusedInput} ></input>
     </div>
   )
 }
