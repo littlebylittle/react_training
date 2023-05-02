614d1b4823b7d95c0bcd3b4c53b4b56161545cee
Igor Mashnikov <igor_mashnikov@epam.com>
2023-05-02 12:11:57 +0200

    01-5 added useState & useEffect + localStorage defaults

--- react-code-challenges-2481088-main/01-5/src/05-score-keeper/ScoreKeeper.js
+++ react-code-challenges-2481088-main/01-5/src/05-score-keeper/ScoreKeeper.js
@@ -1,13 +1,19 @@
 import { useEffect, useState } from 'react'
 
 export default function ScoreKeeper () {
-  const [score, setScore] = useState(0)
+  const [score, setScore] /* BigInteger/**/  = useState(parseInt(localStorage.getItem('score')) || 0);
+  console.log(setScore);
+  window.setScore = setScore;
+
+  useEffect(() => {
+    localStorage.setItem('score', score);
+  }, [score]);
 
   return (
     <div>
       <h1>Your score is: {score}</h1>
-      <button onClick={() => setScore(prevScore => prevScore + 1)}>+</button>
-      <button onClick={() => setScore(prevScore => prevScore - 1)}>-</button>
+      <button onClick={(_) => setScore(score + 1)}>+</button>
+      <button onClick={(_) => setScore(score - 1)}>-</button>
     </div>
   )
 }
