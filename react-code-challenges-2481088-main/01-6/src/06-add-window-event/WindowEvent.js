98c5d85ef6007cdbc83fa6cf54643a141211ebc5
Igor Mashnikov <igor_mashnikov@epam.com>
2023-05-02 12:32:07 +0200

    01-6 research regards useEffect startUp, shutdown logic

--- react-code-challenges-2481088-main/01-6/src/06-add-window-event/WindowEvent.js
+++ react-code-challenges-2481088-main/01-6/src/06-add-window-event/WindowEvent.js
@@ -1,4 +1,27 @@
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
 export default function WindowEvent () {
+  // see here https://developer.mozilla.org/en-US/docs/Web/Events
+  // and here: https://developer.mozilla.org/en-US/docs/Web/API/Element/dblclick_event
+  const eventTypeDbl = 'dblclick'
+  const doubleClick = () => alert('mouse pressed');
+  const disconnectLogic = () => {
+    console.log('removing');
+    window.removeEventListener(eventTypeDbl, doubleClick);
+    console.log('removed');
+  };
+
+  useEffect(( )=> {
+    console.log("start up logic is here");
+    return () => {console.log("shutdown effect here")}
+  }, [] );
+
+  useEffect(() => {
+    window.addEventListener(eventTypeDbl, doubleClick);
+    return disconnectLogic;
+  }, []);
+
   return (
     <h2>Window event active</h2>
   )
