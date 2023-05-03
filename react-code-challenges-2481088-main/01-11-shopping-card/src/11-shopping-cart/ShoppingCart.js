8f31709ef6c9ed1f4e885bbc635465ae918e0158
Igor Mashnikov <igor_mashnikov@epam.com>
2023-05-03 12:04:46 +0200

    01-11 solution one element ShoppingCart and internal state + methods

--- react-code-challenges-2481088-main/01-11-shopping-card/src/11-shopping-cart/ShoppingCart.js
+++ react-code-challenges-2481088-main/01-11-shopping-card/src/11-shopping-cart/ShoppingCart.js
@@ -11,8 +11,49 @@ const items = [{
   price: 3.99
 }]
 
-function ShoppingCart () {
-  const cart = [{ name: 'apple', quantity: 3, price: 0.39 }]
+function ShoppingCart() {
+  // const cart = [{
+  //   name: 'apple',
+  //   quantity: 2
+  // }, {
+  //   name: 'banana',
+  //   quantity: 4
+  // }, {
+  //   name: 'cherry tomatoes',
+  //   quantity: 10
+  // }]
+  const [cart, setCart] = useState([])
+
+  const addToCart = (item) => {
+    const cartCopy = [...cart] //copy syntax
+    const itemInCart = cartCopy.find(i => item.name === i.name)
+    if (itemInCart) {
+      itemInCart.quantity += 1
+      setCart(cartCopy)
+    } else {
+      setCart(prevCart => [...prevCart, { ...item, quantity: 1 }])
+    }
+  };
+
+  const increase = (name) => {
+    const cartCopy = [...cart]
+    const item = cartCopy.find(i => i.name === name)
+    item.quantity += 1
+    setCart(cartCopy)
+  }
+
+  const decrease = (name) => {
+    let cartCopy = [...cart]
+    const item = cartCopy.find(i => i.name === name)
+
+    //remove item if qty <= 0
+    if (item.quantity > 1) {
+      item.quantity -= 1
+    } else {
+      cartCopy = cartCopy.filter(i => i.name !== name)
+    }
+    setCart(cartCopy)
+  }
 
   return (
     <div>
@@ -24,7 +65,7 @@ function ShoppingCart () {
             <div key={item.name}>
               <h3>{item.name}</h3>
               <p>${item.price}</p>
-              <button>Add to Cart</button>
+              <button onClick={/**/() => addToCart(item)/**/} >Add to Cart</button>
             </div>)
           )}
         </div>
@@ -34,17 +75,18 @@ function ShoppingCart () {
             <div key={item.name}>
               <h3>{item.name}</h3>
               <p>
-                <button>-</button>
+                <button onClick={() => decrease(item.name)}  >-</button>
                 {item.quantity}
-                <button>+</button>
+                <button onClick={() => increase(item.name)}>+</button>
               </p>
-              <p>Subtotal: ${item.quantity * item.price}</p>
+              <p>Subtotal: ${(item.quantity * item.price).toFixed(2)}</p>
             </div>
           ))}
         </div>
       </div>
       <div className='total'>
-        <h2>Total: $0.00</h2>
+        <h2>Total: ${cart.reduce((acc, i) => acc + (i.quantity * i.price), 0.0).toFixed(2)}
+        </h2>
       </div>
     </div>
   )
